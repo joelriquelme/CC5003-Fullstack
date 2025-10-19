d97c65bab6cadb444ffd6659d572a041931832b9
taamib <tamara.bravo.c@ug.uchile.cl>
2025-10-18 22:26:31 -0400

    db: conect express to mongodb. refactor(utils): add config and logger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,22 +2,29 @@ import dotenv from "dotenv";
 import express, { NextFunction, Request, Response } from "express";
 import path from "path";
 import mongoose from "mongoose";
+import config from "./utils/config";
+import logger from "./utils/logger";
 
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT;
-const HOST = process.env.HOST || "localhost";
-
 // middlewares
 app.use(express.json());
+
+mongoose.set("strictQuery", false);
+
+if (config.MONGODB_URI) {
+  mongoose.connect(config.MONGODB_URI, { dbName: config.MONGODB_DBNAME }).catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+}
 
 // de prueba
 app.get('/', (req, res) => {
     res.send('¡Backend del torneo Interfacultades funcionando!');
 });
 
-app.listen(Number(PORT), HOST, () => {
-    console.log(`Server running on http://${HOST}:${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
